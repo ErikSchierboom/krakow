6ffaf76559fba4e125bc094daef2b4f1201b601d
Erik Schierboom <erik_schierboom@hotmail.com>
2019-06-01 11:52:20 +0200

    Build CSS file instead of JavaScript

--- src/Krakow.Website/webpack.config.js
+++ src/Krakow.Website/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = (env, options) => {
   const isProduction = options.mode === "production";
@@ -6,7 +7,8 @@ module.exports = (env, options) => {
   return {
     mode: "development",
     entry: {
-      bunlde: ["./src/App.fsproj", "./src/styles/index.scss"]
+      app: ["./src/App.fsproj"],
+      style: ["./src/styles/index.scss"]
     },
     output: {
       path: path.join(__dirname, "./public"),
@@ -24,9 +26,24 @@ module.exports = (env, options) => {
         },
         {
           test: /\.s?css$/,
-          use: ["style-loader", "css-loader", "sass-loader"]
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                hmr: process.env.NODE_ENV === "development"
+              }
+            },
+            "css-loader",
+            "sass-loader"
+          ]
         }
       ]
-    }
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      })
+    ]
   };
 };
