362cdf9b774e4143363bcbb5c649555506a66a13
Erik Schierboom <erik_schierboom@hotmail.com>
2019-06-01 11:43:22 +0200

    Load CSS through webpack

--- src/Krakow.Website/webpack.config.js
+++ src/Krakow.Website/webpack.config.js
@@ -1,22 +1,32 @@
 const path = require("path");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/App.fsproj",
-  output: {
-    path: path.join(__dirname, "./public"),
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: "./public",
-    port: 8888
-  },
-  module: {
-    rules: [
-      {
-        test: /\.fs(x|proj)?$/,
-        use: "fable-loader"
-      }
-    ]
-  }
+module.exports = (env, options) => {
+  const isProduction = options.mode === "production";
+
+  return {
+    mode: "development",
+    entry: {
+      bunlde: ["./src/App.fsproj", "./src/styles/index.scss"]
+    },
+    output: {
+      path: path.join(__dirname, "./public"),
+      filename: "[name].js"
+    },
+    devServer: {
+      contentBase: "./public",
+      port: 8888
+    },
+    module: {
+      rules: [
+        {
+          test: /\.fs(x|proj)?$/,
+          use: "fable-loader"
+        },
+        {
+          test: /\.s?css$/,
+          use: ["style-loader", "css-loader", "sass-loader"]
+        }
+      ]
+    }
+  };
 };
