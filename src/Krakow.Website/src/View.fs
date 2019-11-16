43f7bc9614ccee31591fc46bb4f7c8bb533520dc
Erik Schierboom <erik_schierboom@hotmail.com>
2019-11-16 14:00:52 +0100

    Cleanup view code

--- src/Krakow.Website/src/View.fs
+++ src/Krakow.Website/src/View.fs
@@ -7,7 +7,29 @@ open Krakow.Website.Model
 
 let private byteToHex (b: int) = b.ToString("X2")
 
-let private inputForm model dispatch =
+let private evaluationErrorText error =
+    match error with
+    | WebAssemblyException ex -> sprintf "WebAssembly error: %s" ex.Message
+    | EmptyEquation -> "Empty equation"
+    | InvalidEquation token -> sprintf "Invalid token: %s" token
+    | UnbalancedEquation -> "Equation is unbalanced"
+
+let private evaluationError model =
+    let displayOption =
+        match model.evaluation with
+        | Some(Error _) -> DisplayOptions.Block
+        | _ -> DisplayOptions.None
+
+    let text =
+        match model.evaluation with
+        | Some(Error error) -> evaluationErrorText error
+        | _ -> ""
+
+    mark
+        [ Class "secondary"
+          Style [ Display displayOption ] ] [ str text ]
+
+let private equationForm model dispatch =
     div [ Class "col-sm-4" ]
         [ form [ OnSubmit(fun ev -> ev.preventDefault()) ]
               [ fieldset []
@@ -17,36 +39,35 @@ let private inputForm model dispatch =
                           [ Placeholder "Enter equation..."
                             Value model.equation
                             OnChange(fun ev -> dispatch (UpdateEquation ev.Value)) ]
-                      button [ OnClick(fun _ -> dispatch EvaluateEquation) ] [ str "Evaluate" ] ] ] ]
+                      button [ OnClick(fun _ -> dispatch EvaluateEquation) ] [ str "Evaluate" ]
+                      evaluationError model ] ] ]
 
-let evaluationResult model map =
+let private outputSection header text =
+    [ h3 [ Class "doc" ] [ str header ]
+      p [ Class "doc" ] [ str text ] ]
+
+let private resultSection evaluation =
+    let text = string evaluation.result
+    outputSection "Result" text
+
+let private watSection evaluation = outputSection "WAT" evaluation.wat
+
+let private wasmSection evaluation =
+    let text =
+        evaluation.wasm
+        |> List.map byteToHex
+        |> String.concat " "
+    outputSection "WASM" text
+
+let private resultSections evaluation = resultSection evaluation @ watSection evaluation @ wasmSection evaluation
+
+let private results model =
     match model.evaluation with
-    | Some(Ok evaluation) -> map evaluation
-    | Some(Error error) ->
-        match error with
-        | WebAssemblyException ex -> sprintf "WebAssembly error: %s" ex.Message
-        | EmptyEquation -> "Empty equation"
-        | InvalidEquation token -> sprintf "Invalid token: %s" token
-        | UnbalancedEquation -> "Equation is unbalanced"
-    | None -> ""
-
-let viewOutputHeader name = h3 [ Class "doc" ] [ str name ]
-let viewOutputDetails model map = p [ Class "doc" ] [ str (evaluationResult model map) ]
-
-let output model =
-    div [ Class "col-sm-8" ]
-        [ viewOutputHeader "Result"
-          viewOutputDetails model (fun evaluation -> string evaluation.result)
-          viewOutputHeader "WAT"
-          viewOutputDetails model (fun evaluation -> evaluation.wat)
-          viewOutputHeader "WASM"
-          viewOutputDetails model (fun evaluation ->
-              evaluation.wasm
-              |> List.map byteToHex
-              |> String.concat " ") ]
+    | Some(Ok evaluation) -> div [ Class "col-sm-8" ] (resultSections evaluation)
+    | _ -> div [] []
 
 let view model dispatch =
     div [ Class "container" ]
         [ div [ Class "row" ]
-              [ inputForm model dispatch
-                output model ] ]
+              [ equationForm model dispatch
+                results model ] ]
