08c0f03aba6d9d35de15f7fb3a75bff09951d1d4
Erik Schierboom <erik_schierboom@hotmail.com>
2019-11-18 12:05:55 +0100

    Allow formatting of code

---
+++ format.sh
@@ -0,0 +1,4 @@
+dotnet fantomas --recurse ./src/Krakow.Core
+dotnet fantomas --recurse ./src/Krakow.Repl
+dotnet fantomas --recurse ./src/Krakow.Website/src
+dotnet fantomas --recurse ./test/Krakow.Core.Tests
